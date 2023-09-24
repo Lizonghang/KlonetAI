44e8eac437c7ca01e6ce802074146381a8ce91e4
Lizonghang <870644199@qq.com>
2023-09-24 12:35:10 +0800

    update api key

--- key.py
+++ key.py
@@ -1,1 +1,8 @@
+# Apply on OpenAI: https://platform.openai.com/docs/guides/production-best-practices/api-keys
 OpenAI_API_Key = "<Your-OpenAI-API-Key>"
+
+# Apply on ZhuPuAI: https://open.bigmodel.cn/dev/api#auth
+ZhiPuAI_API_Key = "<Your-ZhiPuAI-API-Key>"
+
+# Apply on Hugging Face: https://huggingface.co/docs/hub/spaces-sdks-docker-argilla#set-up-passwords-and-api-keys-using-secrets-optional
+Huggingface_API_Key = "<Your-Huggingface-API-Key>"

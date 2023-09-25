2ff261d6894b8b7dacddb39b5bf61a00a641616e
Lizonghang <870644199@qq.com>
2023-09-25 17:14:16 +0800

    add api keys for dashscope and sparkdesk

--- key.py
+++ key.py
@@ -1,3 +1,5 @@
+# Not all the API Keys below are required, just choose one of them.
+
 # Apply on OpenAI: https://platform.openai.com/docs/guides/production-best-practices/api-keys
 OpenAI_API_Key = "<Your-OpenAI-API-Key>"
 
@@ -9,3 +11,11 @@ Huggingface_API_Key = "<Your-Huggingface-API-Key>"
 
 # Apply on Baidu AI Studio: https://aistudio.baidu.com/usercenter/token
 AI_Studio_APT_Key = "<Your-AIStudio-API-Key>"
+
+# Apply on Aliyun DashScope: https://help.aliyun.com/zh/dashscope/developer-reference/activate-dashscope-and-create-an-api-key?spm=a2c4g.11186623.0.0.90a174faPZtci1
+DashScope_API_Key = "<Your-DashScope-API-Key>"
+
+# Apply on Xunfeiyun: https://www.xfyun.cn/doc/spark/general_url_authentication.html
+SparkDesk_AppId = "<Your-SparkDesk-APPID>"
+SparkDesk_API_Secret = "<Your-SparkDesk-APISecret>"
+SparkDesk_API_Key = "<Your-SparkDesk-APIKey>"

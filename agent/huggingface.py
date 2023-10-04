935e0a07d00a02417b6debe74094bc90f68ada63
Lizonghang <870644199@qq.com>
2023-10-04 18:27:28 +0800

    support chat mode

---
+++ agent/huggingface.py
@@ -0,0 +1,30 @@
+from transformers.tools import HfAgent
+from transformers.tools.prompts import CHAT_MESSAGE_PROMPT
+
+
+class KAIHfAgent(HfAgent):
+
+    def __init__(self, *args, **kwargs):
+        self._mode = "agent"
+        super().__init__(*args, **kwargs)
+
+    @property
+    def mode(self):
+        return self._mode
+
+    def set_mode(self, mode):
+        self._mode = mode
+
+    def chat(self, task, *, return_code=False, remote=False, **kwargs):
+        if not self._mode == "chat":
+            return super().chat(task, return_code=return_code, remote=remote, **kwargs)
+
+        if self.chat_history is None:
+            description = "\n".join([f"- {name}: {tool.description}" for name, tool in self.toolbox.items()])
+            prompt = self.chat_prompt_template.replace("<<all_tools>>", description)
+        else:
+            prompt = self.chat_history
+        prompt += CHAT_MESSAGE_PROMPT.replace("<<task>>", f"[Chat only] {task}")
+        response = self.generate_one(prompt, stop=["Human:", "====="])
+        self.chat_history = prompt + response.strip() + "\n"
+        self.log(response + "\n")

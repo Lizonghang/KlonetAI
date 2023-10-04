c6e0ad7b70c192c2fd49371c88bb9da4ed543744
Lizonghang <870644199@qq.com>
2023-10-04 18:25:55 +0800

    support chat mode

--- agent/chatglm.py
+++ agent/chatglm.py
@@ -1,8 +1,7 @@
 import zhipuai
 from key import ZhiPuAI_API_Key
 from transformers.tools import Agent
-
-zhipuai.api_key = ZhiPuAI_API_Key
+from transformers.tools.prompts import CHAT_MESSAGE_PROMPT
 
 
 class ChatGLMAgent(Agent):
@@ -12,6 +11,7 @@ class ChatGLMAgent(Agent):
     Args:
         model_name (`str`): The name of the model to be used, could be "chatglm_pro", "chatglm_std", "chatglm_lite",
             or "chatglm_lite_32k".
+        key (`str`, *optional*): The API key obtained from ZhiPuAI Dashboard.
         chat_prompt_template (`str`, *optional*):
             Pass along your own prompt if you want to override the default template for the `chat` method. Can be the
             actual prompt template or a repo ID (on the Hugging Face Hub). The prompt should be in a file named
@@ -32,13 +32,22 @@ class ChatGLMAgent(Agent):
     ```
     """
 
-    def __init__(self, model_name, chat_prompt_template=None, run_prompt_template=None, additional_tools=None):
+    def __init__(self, model_name, key="", chat_prompt_template=None, run_prompt_template=None, additional_tools=None):
         super().__init__(
             chat_prompt_template=chat_prompt_template,
             run_prompt_template=run_prompt_template,
             additional_tools=additional_tools,
         )
         self.model_name = model_name
+        zhipuai.api_key = key or ZhiPuAI_API_Key
+        self._mode = "agent"
+
+    @property
+    def mode(self):
+        return self._mode
+
+    def set_mode(self, mode):
+        self._mode = mode
 
     def generate_one(self, prompt, stop):
         response = zhipuai.model_api.invoke(
@@ -58,3 +67,17 @@ class ChatGLMAgent(Agent):
             if result.endswith(stop_seq):
                 return result[: -len(stop_seq)]
         return result
+
+    def chat(self, task, *, return_code=False, remote=False, **kwargs):
+        if not self._mode == "chat":
+            return super().chat(task, return_code=return_code, remote=remote, **kwargs)
+
+        if self.chat_history is None:
+            description = "\n".join([f"- {name}: {tool.description}" for name, tool in self.toolbox.items()])
+            prompt = self.chat_prompt_template.replace("<<all_tools>>", description)
+        else:
+            prompt = self.chat_history
+        prompt += CHAT_MESSAGE_PROMPT.replace("<<task>>", f"[Chat only] {task}")
+        response = self.generate_one(prompt, stop=["Human:", "====="])
+        self.chat_history = prompt + response.strip() + "\n"
+        self.log(response + "\n")
